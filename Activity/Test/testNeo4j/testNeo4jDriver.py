cc532be30a04520889cb3571e1338d1f78839c2b
Mauricio <mjlomeli@uci.edu>
2019-09-01 23:24:50 -0700

    Markdown file uploaded

--- Activity/Test/testNeo4j/testNeo4jDriver.py
+++ Activity/Test/testNeo4j/testNeo4jDriver.py
@@ -6,7 +6,10 @@ that makes sense on its own, separated from the rest by a newline.
 """
 
 import unittest
-from Activity.Neo4j.Neo4jDriver import insertDict, insertCell, insertStr, insertList, insert
+import getpass
+from nltk.corpus import words
+import random
+from Activity.Neo4j.Neo4jDriver import insertDict, insertCell, insertStr, insertList, insert, openDatabase, closeDatabase
 
 __author__ = "Mauricio Lomeli"
 __date__ = "8/30/2019"
@@ -14,6 +17,13 @@ __maintainer__ = "Mauricio Lomeli"
 __email__ = "mjlomeli@uci.edu"
 __status__ = "Prototype"
 __version__ = "0.0.0.2"
+
+
+class Cell:
+    def __init__(self, content=None, header=None, id=1):
+        self.content = content
+        self.header = header
+        self.id = id
 
 
 def testNeo4j():
@@ -24,22 +34,51 @@ def testNeo4j():
 class TestNeo4jDriver(unittest.TestCase):
 
     def setUp(self):
-        # TODO: write what needs to be instantiated for each test
-        neo4jdriver = None
-        pass
+        self.uri = input("Enter your Neo4j URI: ")
+        self.username = input("Enter your username: ")
+        self.password = getpass.getpass("Enter your Neo4j password: ")
+        printProgressBar(0, 1, "Opening Database")
+        self.neo4jdriver = openDatabase(self.uri, self.username, self.password)
+        printProgressBar(1, 1, "Opening Database")
 
-    def test_func(self):
-        # TODO: write your code here
-        pass
+    def test_insertStr(self):
+        start = 0
+        end = 2000
+        word_list = words.words()
+        for count in range(end):
+            try:
+                from_word_list = random.choice(word_list)
+                insertStr(from_word_list)
+                start += 1
+            except Exception as e:
+                self.fail("Failed with inserting into insertStr: " + from_word_list)
+
+
+
+    def test_insertCell(self):
+        start = 0
+        end = 2000
+        word_list = words.words()
+        for count in range(end):
+            try:
+                header = random.choice(word_list)
+                content = random.choice(word_list)
+                insertStr(from_word_list)
+                start += 1
+            except Exception as e:
+                self.fail("Failed with inserting into insertStr: " + from_word_list)
+
+
 
     def tearDown(self):
-        # TODO: write what to do after a test is finished (close files?)
-        pass
+        printProgressBar(0, 1, "Closing Database")
+        closeDatabase(self.neo4jdriver[0])
+        printProgressBar(1, 1, "Closing Database")
 
 
 def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=50, fill='█'):
     """ 
-    Displays a progress bar for each iteration.
+    Displays a progress bar for each iteration. I've added my own personal twist to make it more functional.
     Title: Progress Bar
     Author: Benjamin Cordier
     Date: 6/10/2019
