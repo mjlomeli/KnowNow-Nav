c1ae2bffa28719df0a0e0b0625d861fda51b783d
mjlomeli <mjlomeli@uci.edu>
2019-09-06 10:55:09 -0700

    Finished NLP create index

--- Activity/NLP/Similarity.py
+++ Activity/NLP/Similarity.py
@@ -24,8 +24,8 @@ __email__ = "mjlomeli@uci.edu"
 __status__ = "Prototype"
 
 _data = Path.cwd() / Path('data') / Path('scores.pickle')
-_IDF_WEIGHTING = False  # must set to True to run original program
-_TESTING_WEIGHTING = True
+__idf__weighting = False  # must set to True to run original program
+_TESTING__weighting = True
 _corpus = Spreadsheet()  # must add your file/items here like your corpus
 _index = {'antony': {0: 157, 1: 73, 2: 0, 3: 0, 4: 0, 5: 0},
          'brutus': {0: 4, 1: 157, 2: 0, 3: 1, 4: 0, 5: 0},
@@ -61,83 +61,70 @@ def query(query_list, index=file()):
 """
 
 
-def _prob_idf(term, index=_index):
+def __prob__idf(term, index=_index):
     df_t = sum([1 for x in list(index[term].values()) if x > 0])
     corp_size = len(list(_index.values())[0]) if len(_index) > 0 else 0
-    if _TESTING_WEIGHTING:
+    if _TESTING__weighting:
         corp_size = 37
     diff = corp_size - df_t
     return log10(diff / df_t) if diff > 0 else 0
 
 
-def _score(term, row, index=_index):
+def __score(term, row, index=_index):
     if isinstance(term, str) and term in index:
         tf = index[term][row] if row in index[term] else 0
         return (1 + log10(tf)) if tf > 0 else 0
     elif isinstance(term, list):
-        return _weighting(term, row, index)
+        return __weighting(term, row, index)
 
 
-"""
-def _score(term, row, index=_index):
-    if isinstance(term, str) and term in index:
-        tf = index[term][row] if row in index[term] else 0
-        return (1 + log10(tf)) if tf > 0 else 0
-    elif isinstance(term, list):
-        score = 0
-        for word in term:
-            score += _score(word, row, index)
-        return score
-"""
-
-
-def _idf(term, index=_index, ):
+def __idf(term, index=_index, ):
     if isinstance(term, str):
         if term in index:
             n = len(list(index.values())[0]) if len(index) > 0 else 0
-            if _TESTING_WEIGHTING:
+            if _TESTING__weighting:
                 n = 37
             df_t = sum([1 for x in list(index[term].values()) if x > 0])
             return log10(n / df_t) if df_t > 0 else 0
         else:
             return 0
     else:
-        raise TypeError('term in _idf function must be a string.')
+        raise TypeError('term in __idf function must be a string.')
 
 
-def _weighting(term, row, index=_index):
+def __weighting(term, row, index=_index):
     if isinstance(term, str):
-        if _IDF_WEIGHTING:
-            return _score(term, row, index) * _idf(term, index)
+        if __idf__weighting:
+            return __score(term, row, index) * __idf(term, index)
         else:
-            return _score(term, row, index)
+            return __score(term, row, index)
     elif isinstance(term, list):
         score = 0
         for word in term:
-            if _IDF_WEIGHTING:
-                score += _score(word, row, index) * _idf(word, index)
+            if __idf__weighting:
+                score += __score(word, row, index) * __idf(word, index)
             else:
-                score += _score(word, row, index)
+                score += __score(word, row, index)
         return score
     else:
-        raise TypeError('term in function _weighting must be a string or list of strings.')
+        raise TypeError('term in function __weighting must be a string or list of strings.')
 
 
-def _length_norm(term, row, index=_index):
+def __length_norm(term, row, index=_index):
     if term in index and row in index[term]:
-        d_term = _weighting(term, row, index) if _IDF_WEIGHTING else _score(term, row, index)
-        if _IDF_WEIGHTING:
-            d_norm = sqrt(sum([_weighting(words, row, index) ** 2 for words in index.keys()]))
+        d_term = __weighting(term, row, index) if __idf__weighting else __score(term, row, index)
+        if __idf__weighting:
+            d_norm = sqrt(sum([__weighting(words, row, index) ** 2 for words in index.keys()]))
         else:
-            d_norm = sqrt(sum([_score(words, row, index) ** 2 for words in index.keys()]))
+            d_norm = sqrt(sum([__score(words, row, index) ** 2 for words in index.keys()]))
         if d_norm > 0:
             return d_term / d_norm
         else:
             return 0
 
 
-def _cosine(row1, row2, index=_index):
-    return sum([_length_norm(word, row1, index) * _length_norm(word, row2, index) for word in index.keys()])
+def __cosine(row1, row2, index=_index):
+    return sum([__length_norm(word, row1, index) * __length_norm(word, row2, index) for word in index.keys()])
 
 
 def cosine_score(query, index=_index, max_results=_max_postings_size):
@@ -150,21 +137,21 @@ def cosine_score(query, index=_index, max_results=_max_postings_size):
     if isinstance(query, str):
         query = [query]
     corp_size = len(list(_index.values())[0]) if len(_index) > 0 else 0
-    if _TESTING_WEIGHTING:
+    if _TESTING__weighting:
         corp_size = 37
     scores = [0] * corp_size
     length = len(index[list(index.keys())[0]])
     doc_word_count = [sum([index[item][i] for item in index.keys() if index[item][i] > 0]) for i in range(length)]
     for term in query:
-        w_q = _weight_query(term, query, index)
+        w_q = __weight_query(term, query, index)
         for i in range(length):
-            scores[i] += _weighting(term, i, index) * w_q
+            scores[i] += __weighting(term, i, index) * w_q
     for i in range(length):
         scores[i] = scores[i] / doc_word_count[i]
     return nlargest(max_results, list(zip(scores, [i for i in range(length)])))
 
 
-def _weight_query(term, query, index=_index):
+def __weight_query(term, query, index=_index):
     if isinstance(query, str):
         query = [query]
     q_index = {}
@@ -177,7 +164,7 @@ def _weight_query(term, query, index=_index):
         else:
             q_index[tok] = {0: tf[tok]}
 
-    return _weighting(term, 0, q_index)
+    return __weighting(term, 0, q_index)
 
 
 def __increment_index(index, val):
@@ -342,7 +329,7 @@ def testIndex():
 
 
 def testSimilarity():
-    if _IDF_WEIGHTING:
+    if __idf__weighting:
         index = {'antony':      {0: 157,    1: 73,  2: 0, 3: 0, 4: 0, 5: 0},
                  'brutus':      {0: 4,      1: 157, 2: 0, 3: 1, 4: 0, 5: 0},
                  'caesar':      {0: 232,    1: 227, 2: 0, 3: 2, 4: 1, 5: 1},
@@ -351,18 +338,18 @@ def testSimilarity():
                  'mercy':       {0: 2,      1: 0,   2: 3, 3: 5, 4: 5, 5: 1},
                  'worser':      {0: 2,      1: 0,   2: 1, 3: 1, 4: 1, 5: 0}}
 
-        result = _weighting('antony', 0, index)
-        assert abs(result - 4.05) < 0.01, '_weighting(antony,0, index) must be about 4.05, result=' + str(result)
-        result = _weighting('brutus', 0, index)
-        assert abs(result - 1.75) < 0.01, '_weighting(brutus,0, index) must be about 1.75, result=' + str(result)
-        result = _weighting('caesar', 0, index)
-        assert abs(result - 2.93) < 0.01, '_weighting(caesar,0, index) must be about 2.93, result=' + str(result)
-        result = _score(['brutus', 'caesar'], 0, index)
-        assert abs(result - 4.67) < 0.01, '_score([brutus,caesar],0, index) must be about 4.67, result=' + str(result)
-        result = _weighting(['brutus', 'caesar'], 0, index)
-        assert abs(result - 4.67) < 0.01, '_weighting([brutus,caesar],0, index) must be about 4.67, result=' + str(result)
+        result = __weighting('antony', 0, index)
+        assert abs(result - 4.05) < 0.01, '__weighting(antony,0, index) must be about 4.05, result=' + str(result)
+        result = __weighting('brutus', 0, index)
+        assert abs(result - 1.75) < 0.01, '__weighting(brutus,0, index) must be about 1.75, result=' + str(result)
+        result = __weighting('caesar', 0, index)
+        assert abs(result - 2.93) < 0.01, '__weighting(caesar,0, index) must be about 2.93, result=' + str(result)
+        result = __score(['brutus', 'caesar'], 0, index)
+        assert abs(result - 4.67) < 0.01, '__score([brutus,caesar],0, index) must be about 4.67, result=' + str(result)
+        result = __weighting(['brutus', 'caesar'], 0, index)
+        assert abs(result - 4.67) < 0.01, '__weighting([brutus,caesar],0, index) must be about 4.67, result=' + str(result)
         print('\033[1m\033[92m' + '100% passed' + '\033[0m')
-        print('\033[90m' + 'To run cosine similarity test, set _IDF_WEIGHTING = False' + '\033[0m')
+        print('\033[90m' + 'To run cosine similarity test, set __idf__weighting = False' + '\033[0m')
         print('\033[90m' + 'Then rerun this test.' + '\033[0m')
     else:
         index = {
@@ -371,24 +358,24 @@ def testSimilarity():
             'gossip': {0: 2, 1: 0, 2: 6},
             'wuthering': {0: 0, 1: 0, 2: 38}}
 
-        result = _length_norm('affection', 0, index)
-        assert abs(result - 0.789) < 0.01, '_length_norm(affection,0) must be about 0.78, result=' + str(result)
-        result = _length_norm('jealous', 0, index)
-        assert abs(result - 0.515) < 0.01, '_length_norm(jealous,0) must be about 0.515, result=' + str(result)
-        result = _length_norm('gossip', 0, index)
-        assert abs(result - 0.335) < 0.01, '_length_norm(gossip,0) must be about 0.335, result=' + str(result)
-        result = _weighting('wuthering', 0, index)
-        assert abs(result - 0) < 0.01, '_weighting() Must be about 0, result=' + str(result)
+        result = __length_norm('affection', 0, index)
+        assert abs(result - 0.789) < 0.01, '__length_norm(affection,0) must be about 0.78, result=' + str(result)
+        result = __length_norm('jealous', 0, index)
+        assert abs(result - 0.515) < 0.01, '__length_norm(jealous,0) must be about 0.515, result=' + str(result)
+        result = __length_norm('gossip', 0, index)
+        assert abs(result - 0.335) < 0.01, '__length_norm(gossip,0) must be about 0.335, result=' + str(result)
+        result = __weighting('wuthering', 0, index)
+        assert abs(result - 0) < 0.01, '__weighting() Must be about 0, result=' + str(result)
 
-        result = _cosine(0, 1, index)
-        assert abs(result - 0.94) < 0.01, '_cosine(0,1) must be about 0.94, result=' + str(result)
-        result = _cosine(0, 2, index)
-        assert abs(result - 0.79) < 0.01, '_cosine(0,2) must be about 0.79, result=' + str(result)
-        result = _cosine(1, 2, index)
-        assert abs(result - 0.69) < 0.01, '_length_norm(1,2) must be about 0.69, result=' + str(result)
+        result = __cosine(0, 1, index)
+        assert abs(result - 0.94) < 0.01, '__cosine(0,1) must be about 0.94, result=' + str(result)
+        result = __cosine(0, 2, index)
+        assert abs(result - 0.79) < 0.01, '__cosine(0,2) must be about 0.79, result=' + str(result)
+        result = __cosine(1, 2, index)
+        assert abs(result - 0.69) < 0.01, '__length_norm(1,2) must be about 0.69, result=' + str(result)
 
         print('\033[1m\033[92m' + '100% passed' + '\033[0m')
-        print('\033[90m' + 'To run tf-idf weighting similarity test, set _IDF_WEIGHTING = True' + '\033[0m')
+        print('\033[90m' + 'To run tf-idf weighting similarity test, set __idf__weighting = True' + '\033[0m')
         print('\033[90m' + 'Then rerun this test.' + '\033[0m')
 
 
